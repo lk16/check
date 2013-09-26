fec32ddf0b62bb21423ac0f848da1f3e775d5a5b
Luuk Verweij <luuk_verweij@msn.com>
2013-09-26 18:15:20 +0200

    added ./CMakeLists.txt and other local changes

--- src/main/check.cpp
+++ src/main/check.cpp
@@ -2,58 +2,6 @@
 
 #include "gui/main_window.hpp"
 
-void run_no_gui(){
-#if 0 
-  board *b;
-  bot_base* bots[2];
-  std::string instring;
-  int inint;
-
- 
-  instring = "";
-  inint = -1;
-  bots[0] = bots[1] = NULL;
-  b = new board();
-  b->reset();
-  
-  
-  while(true){
-    std::cout << "Is the black player a human? ";
-    std::cin >> instring;
-    switch((char)instring.substr(0,1)){
-      case 'y':
-      case 'Y':
-        std::cout << '\n';
-        break;
-      case 'n':
-      case 'N':
-        std::cout << "Enter searchdepth of black player. "; 
-        std::cin >> inint;
-        bots[BLACK] = new bot_ali(BLACK,inint,2*inint);
-      default:
-        continue;
-    }
-  }
-  while(true){
-    std::cout << "Is the white player a human? ";
-    std::cin >> instring;
-    switch((char)instring.substr(0,1)){
-      case 'y':
-      case 'Y':
-        std::cout << '\n';
-        break;
-      case 'n':
-      case 'N':
-        std::cout << "Enter searchdepth of white player. "; 
-        std::cin >> inint;
-        bots[WHITE] = new bot_ali(WHITE,inint,2*inint);
-      default:
-        continue;
-    }
-  }
-#endif
-}
-
 void run_debug(){
   board b;
   std::cout << b.get_stable_disc_count_diff(3) << std::endl;
